28e2e8cd50f00aab49ea2e62472c1a348ed44cf9
Bipul Chaudhary <bipulchau@gmail.com>
2022-09-15 19:54:59 -0400

    checks if the string length is not equal to 6

--- checkIfStingIsHexCode.py
+++ checkIfStingIsHexCode.py
@@ -5,20 +5,35 @@ def checkIfStringIsHexCode(string):
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f']
     digit.extend(alphabet)
     result_list = []
-    print(digit)
+    # print(digit)
     # print(str[0])
-    if string[0] != '#' and len != 6:
+    if string[0] != '#':
+        return False
+    elif length_of_string != 6:
         return False
     else:
         for i in new_str:
             if i in digit:
-                print(i)
+                # print(i)
                 result_list.append(True)
             else:
-                print(i)
+                # print(i)
                 result_list.append(False)
-    print(result_list)
+    # print(result_list)
 
-print(checkIfStringIsHexCode("#CD5C5C"))
-# print(checkIfStringIsHexCode("CD5C5C"))
-# print(checkIfStringIsHexCode("#CD5C&C"))
+    if False in result_list:
+        return False
+    else:
+        return True
+
+print(checkIfStringIsHexCode("#CD5C5C")) # True
+print(checkIfStringIsHexCode("#EAECEE")) # True
+print(checkIfStringIsHexCode("#eaecee")) # True
+print(checkIfStringIsHexCode("#CD5C58C")) # False
+print(checkIfStringIsHexCode("#CD5C5Z")) # False
+print(checkIfStringIsHexCode("#CD5C&C")) # False
+print(checkIfStringIsHexCode("CD5C5C")) # False
+
+
+
+
