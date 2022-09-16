026b7e106cad3b3b7e6af6721a078831cd7e9a65
Bipul Chaudhary <bipulchau@gmail.com>
2022-09-15 20:55:39 -0400

    checks if the number is a prime number and returns next prime number

--- returnTheNextPrime.py
+++ returnTheNextPrime.py
@@ -3,24 +3,22 @@ def check_prime_number(number):
         for i in range(2, int(number / 2) + 1):
             if number % i == 0:
                 return False
-            else:
-                return True
+        return True
+    return False
 
 
-def return_the_next_prime(number):
+def next_prime_num(number):
     if check_prime_number(number):
         return number
     else:
         number += 1
-        return_the_next_prime(number)
-        # print(number)
-
+        return next_prime_num(number)
 
 
 print(check_prime_number(12))  # False
 print(check_prime_number(24))  # False
-print(check_prime_number(11))  # True
+print(check_prime_number(11), end="\n\n")  # True
 
-print(return_the_next_prime(12)) # 13
-print(return_the_next_prime(24)) # 25
-print(return_the_next_prime(11)) # 11
+print(next_prime_num(12))  # 13
+print(next_prime_num(24))  # 29
+print(next_prime_num(11))  # 11
